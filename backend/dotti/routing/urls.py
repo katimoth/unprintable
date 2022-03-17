b6ecb0db293f7f3e0fcc9aaecbf05a252ca6d6e5
Tim Kao <katimoth@umich.edu>
2022-03-17 19:04:37 +0000

    getsong route

--- backend/dotti/routing/urls.py
+++ backend/dotti/routing/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getsong/', views.getsong, name='getsong'),
+    path('postaccuracy/', views.postaccuracy, name='postaccuracy'),
+    path('getmetrics/', views.getmetrics, name='getmetrics'),
 ]
