22d57c2648daae2fac61710fdfe15b2f47b5ad09
evgriff <evgriff@umich.edu>
2022-03-18 22:05:52 -0400

    login page aesthetic improvements

--- dotti/dotti/TitleView.swift
+++ dotti/dotti/TitleView.swift
@@ -12,40 +12,56 @@ struct TitleView: View {
     var body: some View {
         ZStack {
             LinearGradient(
-                gradient: Gradient(colors: [Color(red: 252 / 255, green: 209 / 255, blue: 162 / 255), .white]),
+                gradient: Gradient(colors: [Color(red: 252 / 255, green: 209 / 255, blue: 162 / 255), Color(red: 252 / 255, green: 209 / 255, blue: 162 / 255), .white]),
                 startPoint: .top,
                 endPoint: .bottom
             ).edgesIgnoringSafeArea(.all)
             VStack {
                 Group {
-                    Image("dotti_transparent.png")
-                        .resizable()
-                        .scaledToFit()
-                    Text("dotti")
-                        .font(.title)
+                    Spacer()
+                    Group {
+                        Image("icon-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150.0, height: 150.0)
+                        Text("dotti")
+                            .font(.title)
+                        
+                    }
+                    Spacer()
                     HStack{
                         Button(action: {
                             print("tapped!")
                         }, label: {
-                            Text("Login")
-                                .foregroundColor(.white)
-                                .frame(width: 100, height: 40)
-                                .background(Color.green)
-                                .cornerRadius(15)
+                            Text("**REGISTER**")
+                                .foregroundColor(Color(red: 52 / 255, green: 28 / 255, blue: 9 / 255))
+                                .frame(width: 120, height: 40)
+                                .background(.clear)
+                                .cornerRadius(5)
+                                .font(
+                                    .system(size: 16)
+                                )
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 5.0)
+                                        .stroke(Color(red: 52 / 255, green: 28 / 255, blue: 9 / 255), lineWidth: 2.0)
+                                )
                                 .padding()
                         })
                         Button(action: {
                             print("tapped!")
                         }, label: {
-                            Text("Register")
+                            Text("**LOGIN**")
                                 .foregroundColor(.white)
-                                .frame(width: 100, height: 40)
-                                .background(Color.green)
-                                .cornerRadius(15)
+                                .frame(width: 180, height: 40)
+                                .background(Color(red: 52 / 255, green: 28 / 255, blue: 9 / 255))
+                                .font(
+                                    .system(size: 16)
+                                )
+                                .cornerRadius(5)
                                 .padding()
                         })
                     }
-                }.frame(maxHeight: .infinity, alignment: .bottom)
+                }//.frame(maxHeight: .infinity, alignment: .bottom)
             }
         }
     }
