5319ff8c1685d79421a74fe1a58e23d40ee3fcde
slowhei <ichikawt@umich.edu>
2022-04-04 23:53:59 -0400

    Began work on UI of `GuitarLessonView`

--- dotti/dotti/GuitarLessonView.swift
+++ dotti/dotti/GuitarLessonView.swift
@@ -10,8 +10,21 @@ import SwiftUI
 /// - Invariant: UI orientation must always be landscape
 struct GuitarLessonView: View {
     var body: some View {
-        Group{
-            Text("Hello, world!")
+        ZStack {
+            HStack(spacing: 0) {
+                Text("[insert camera feed here]")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding()
+                    .foregroundColor(Color.american_bronze)
+                    .background(Color.deep_champagne)
+                GroupBox {
+                    Text("Next Chord")
+                }
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.ruber)
+            }
+
         }
             // This view is landscape orientation only
             .onAppear { 
